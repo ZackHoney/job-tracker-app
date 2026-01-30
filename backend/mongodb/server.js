f65b75a46934c45d52d729e7336d0dad99faed9b
Zack Honey <zhoney466@gmail.com>
2026-01-29 19:55:51 -0800

    Created mongodb database and connected it to app

---
+++ backend/mongodb/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri);
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected successfully to MongoDB');
+        return client.db();
+    } catch (error) {
+        console.error('Error connecting to the database:', error.message);
+        process.exit(1);
+    }
+}
+
+module.exports ={ connectToDatabase, client};
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+// server.js
+
+// Load environment variables from .env file
+require('dotenv').config();
+const { connectToDatabase } = require('./mongodb/server.js');
+
+
+// Now you can access process.env.PORT
+const PORT = process.env.PORT || 8000; // Use the value from .env, or a fallback (e.g., 8000)
+
+const express = require('express');
+const app = express();
+
+// ... your other server configurations ...
+connectToDatabase();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
